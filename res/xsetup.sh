06550a0df4e2997415c8691b2e812d0e5bfd9df7
/TheDevKiller <39555268+TheDevKiller@users.noreply.github.com>
2019-08-21 00:28:19 +0200

    Improve xsetup.sh (profile, Xresources, xinitrc.d)

--- res/xsetup.sh
+++ res/xsetup.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
+. /etc/profile
+. ~/.profile
+. /etc/xprofile
+. ~/.xprofile
+
+xrdb -merge /etc/X11/Xresources
+xrdb -merge ~/.Xresources
+
+xinitdir="/etc/X11/xinit/xinitrc.d"
+for script in $xinitdir/*; do
+    . "$script"
+done
+
 ~/.xsession
+
 exec $@
