463644549b18d978e4f333c252fe4dfc1af3c45a
catdotjs <catdotjs@gmail.com>
2023-12-18 15:27:34 +0200

    added bigclock_format to config.ini

--- src/draw.c
+++ src/draw.c
@@ -240,11 +240,11 @@ void alpha_blit(struct tb_cell* buf, uint16_t x, uint16_t y, uint16_t w, uint16_
 
 void draw_bigclock(struct term_buf* buf)
 {
-	if (!config.bigclock)
+  uint32_t bigclocklength = strlen(config.bigclock_format);
+	if (!config.bigclock || bigclocklength == 0)
     {
         return;
     }
-  uint32_t bigclocklength = strlen(config.bigclock_format);
 	
   int xo = (buf->width / 2) - bigclocklength * (CLOCK_W+1) / 2;
 	int yo = (buf->height - buf->box_height) / 2 - CLOCK_H - 2;
