02c88351ea73894910dff6a850ee3d29042016de
xelantro <40129767+xelantro@users.noreply.github.com>
2024-02-13 00:33:48 +0000

    remove setting of LANG env var completly (lang is setup with a default value earlier)

--- src/login.c
+++ src/login.c
@@ -220,11 +220,6 @@ void env_init(struct passwd* pwd)
 	setenv("SHELL", pwd->pw_shell, 1);
 	setenv("USER", pwd->pw_name, 1);
 	setenv("LOGNAME", pwd->pw_name, 1);
-
-	if (lang)
-	{
-		setenv("LANG", lang, 1);
-	}
 	
 	// Set PATH if specified in the configuration
 	if (strlen(config.path))
