19d4b195f323f67b8426f10db79dfad56b23db30
Moabeat <moabeat@berlin.de>
2024-07-28 13:02:42 +0200

    Display error messages differently in info line (#661)
    * Add list of error messages to InfoLine.zig
    
    * Change info and error cases according to review
    
    * Add changes from review for width calculation

--- src/main.zig
+++ src/main.zig
@@ -62,7 +62,8 @@ pub fn main() !void {
     var config: Config = undefined;
     var lang: Lang = undefined;
     var save: Save = undefined;
-    var info_line = InfoLine{};
+    var info_line = InfoLine.init(allocator);
+    defer info_line.deinit();
 
     if (res.args.help != 0) {
         try clap.help(stderr, clap.Help, &params, .{});
@@ -100,7 +101,11 @@ pub fn main() !void {
         const config_path = try std.fmt.allocPrint(allocator, "{s}{s}config.ini", .{ s, trailing_slash });
         defer allocator.free(config_path);
 
-        config = config_ini.readFileToStructWithMap(config_path, mapped_config_fields) catch Config{};
+        config = config_ini.readFileToStructWithMap(config_path, mapped_config_fields) catch _config: {
+            // literal error message, due to language file not yet available
+            try info_line.addError("unable to parse config file");
+            break :_config Config{};
+        };
 
         const lang_path = try std.fmt.allocPrint(allocator, "{s}{s}lang/{s}.ini", .{ s, trailing_slash, config.lang });
         defer allocator.free(lang_path);
@@ -115,7 +120,11 @@ pub fn main() !void {
             save = save_ini.readFileToStruct(save_path) catch migrator.tryMigrateSaveFile(&user_buf, config.save_file);
         }
     } else {
-        config = config_ini.readFileToStructWithMap(build_options.data_directory ++ "/config.ini", mapped_config_fields) catch Config{};
+        config = config_ini.readFileToStructWithMap(build_options.data_directory ++ "/config.ini", mapped_config_fields) catch _config: {
+            // literal error message, due to language file not yet available
+            try info_line.addError("unable to parse config file");
+            break :_config Config{};
+        };
 
         const lang_path = try std.fmt.allocPrint(allocator, "{s}/lang/{s}.ini", .{ build_options.data_directory, config.lang });
         defer allocator.free(lang_path);
@@ -127,6 +136,9 @@ pub fn main() !void {
             save = save_ini.readFileToStruct(save_path) catch migrator.tryMigrateSaveFile(&user_buf, config.save_file);
         }
     }
+
+    info_line.error_bg = config.error_bg;
+    info_line.error_fg = config.error_fg;
 
     if (!build_options.enable_x11_support) try info_line.setText(lang.no_x11_support);
 
@@ -178,13 +190,13 @@ pub fn main() !void {
     defer desktop.deinit();
 
     desktop.addEnvironment(.{ .Name = lang.shell }, "", .shell) catch {
-        try info_line.setText(lang.err_alloc);
+        try info_line.addError(lang.err_alloc);
     };
 
     if (build_options.enable_x11_support) {
         if (config.xinitrc) |xinitrc| {
             desktop.addEnvironment(.{ .Name = lang.xinitrc, .Exec = xinitrc }, "", .xinitrc) catch {
-                try info_line.setText(lang.err_alloc);
+                try info_line.addError(lang.err_alloc);
             };
         }
     }
@@ -227,10 +239,10 @@ pub fn main() !void {
         switch (active_input) {
             .session => desktop.handle(null, insert_mode),
             .login => login.handle(null, insert_mode) catch {
-                try info_line.setText(lang.err_alloc);
+                try info_line.addError(lang.err_alloc);
             },
             .password => password.handle(null, insert_mode) catch {
-                try info_line.setText(lang.err_alloc);
+                try info_line.addError(lang.err_alloc);
             },
         }
     }
@@ -274,7 +286,7 @@ pub fn main() !void {
     // Switch to selected TTY if possible
     open_console_dev: {
         const fd = std.posix.open(config.console_dev, .{ .ACCMODE = .WRONLY }, 0) catch {
-            try info_line.setText(lang.err_console_dev);
+            try info_line.addError(lang.err_console_dev);
             break :open_console_dev;
         };
         defer std.posix.close(fd);
@@ -310,10 +322,10 @@ pub fn main() !void {
                 switch (config.animation) {
                     .none => {},
                     .doom => doom.realloc() catch {
-                        try info_line.setText(lang.err_alloc);
+                        try info_line.addError(lang.err_alloc);
                     },
                     .matrix => matrix.realloc() catch {
-                        try info_line.setText(lang.err_alloc);
+                        try info_line.addError(lang.err_alloc);
                     },
                 }
 
@@ -362,10 +374,10 @@ pub fn main() !void {
                 switch (active_input) {
                     .session => desktop.handle(null, insert_mode),
                     .login => login.handle(null, insert_mode) catch {
-                        try info_line.setText(lang.err_alloc);
+                        try info_line.addError(lang.err_alloc);
                     },
                     .password => password.handle(null, insert_mode) catch {
-                        try info_line.setText(lang.err_alloc);
+                        try info_line.addError(lang.err_alloc);
                     },
                 }
 
@@ -386,7 +398,7 @@ pub fn main() !void {
                 buffer.drawLabel(lang.login, label_x, label_y + 4);
                 buffer.drawLabel(lang.password, label_x, label_y + 6);
 
-                info_line.draw(buffer);
+                try info_line.draw(buffer);
 
                 if (!config.hide_key_hints) {
                     var length: u64 = 0;
@@ -439,7 +451,7 @@ pub fn main() !void {
 
                 draw_lock_state: {
                     const lock_state = interop.getLockState(config.console_dev) catch {
-                        try info_line.setText(lang.err_console_dev);
+                        try info_line.addError(lang.err_console_dev);
                         break :draw_lock_state;
                     };
 
@@ -515,7 +527,7 @@ pub fn main() !void {
                     }
                 } else if (pressed_key == brightness_down_key and unistd.access(&config.brightnessctl[0], unistd.X_OK) == 0) brightness_change: {
                     const brightness_str = std.fmt.allocPrint(allocator, "{s}%-", .{config.brightness_change}) catch {
-                        try info_line.setText(lang.err_brightness_change);
+                        try info_line.addError(lang.err_brightness_change);
                         break :brightness_change;
                     };
                     defer allocator.free(brightness_str);
@@ -523,7 +535,7 @@ pub fn main() !void {
                     _ = brightness.spawnAndWait() catch .{};
                 } else if (pressed_key == brightness_up_key and unistd.access(&config.brightnessctl[0], unistd.X_OK) == 0) brightness_change: {
                     const brightness_str = std.fmt.allocPrint(allocator, "+{s}%", .{config.brightness_change}) catch {
-                        try info_line.setText(lang.err_brightness_change);
+                        try info_line.addError(lang.err_brightness_change);
                         break :brightness_change;
                     };
                     defer allocator.free(brightness_str);
@@ -595,7 +607,7 @@ pub fn main() !void {
 
                     try info_line.setText(lang.authenticating);
                     InfoLine.clearRendered(allocator, buffer) catch {};
-                    info_line.draw(buffer);
+                    try info_line.draw(buffer);
                     _ = termbox.tb_present();
 
                     session_pid = try std.posix.fork();
@@ -616,7 +628,7 @@ pub fn main() !void {
                 if (auth_err) |err| {
                     auth_fails += 1;
                     active_input = .password;
-                    try info_line.setText(getAuthErrorMsg(err, lang));
+                    try info_line.addError(getAuthErrorMsg(err, lang));
                     if (config.clear_password or err != error.PamAuthError) password.clear();
                 } else {
                     password.clear();
@@ -665,10 +677,10 @@ pub fn main() !void {
                 switch (active_input) {
                     .session => desktop.handle(&event, insert_mode),
                     .login => login.handle(&event, insert_mode) catch {
-                        try info_line.setText(lang.err_alloc);
+                        try info_line.addError(lang.err_alloc);
                     },
                     .password => password.handle(&event, insert_mode) catch {
-                        try info_line.setText(lang.err_alloc);
+                        try info_line.addError(lang.err_alloc);
                     },
                 }
                 update = true;
--- src/tui/components/InfoLine.zig
+++ src/tui/components/InfoLine.zig
@@ -2,23 +2,65 @@ const std = @import("std");
 const utils = @import("../utils.zig");
 const TerminalBuffer = @import("../TerminalBuffer.zig");
 
+const ArrayList = std.ArrayList;
+const ErrorMessage = struct { width: u8, text: []const u8 };
+
 const InfoLine = @This();
 
-text: []const u8 = "",
-width: u8 = 0,
+error_list: ArrayList(ErrorMessage),
+error_bg: u16,
+error_fg: u16,
+text: []const u8,
+width: u8,
+
+pub fn init(allocator: std.mem.Allocator) InfoLine {
+    return .{
+        .error_list = ArrayList(ErrorMessage).init(allocator),
+        .error_bg = 0,
+        .error_fg = 258,
+        .text = "",
+        .width = 0,
+    };
+}
+
+pub fn deinit(self: InfoLine) void {
+    self.error_list.deinit();
+}
 
 pub fn setText(self: *InfoLine, text: []const u8) !void {
     self.width = if (text.len > 0) try utils.strWidth(text) else 0;
     self.text = text;
 }
 
-pub fn draw(self: InfoLine, buffer: TerminalBuffer) void {
-    if (self.width > 0 and buffer.box_width > self.width) {
-        const label_y = buffer.box_y + buffer.margin_box_v;
-        const x = buffer.box_x + ((buffer.box_width - self.width) / 2);
-
-        buffer.drawLabel(self.text, x, label_y);
+pub fn addError(self: *InfoLine, error_message: []const u8) !void {
+    if (error_message.len > 0) {
+        const entry = .{
+            .width = try utils.strWidth(error_message),
+            .text = error_message,
+        };
+        try self.error_list.append(entry);
     }
+}
+
+pub fn draw(self: InfoLine, buffer: TerminalBuffer) !void {
+    var text: []const u8 = self.text;
+    var bg: u16 = buffer.bg;
+    var fg: u16 = buffer.fg;
+    var width: u8 = self.width;
+
+    if (self.error_list.items.len > 0) {
+        const entry = self.error_list.getLast();
+        text = entry.text; 
+        bg = self.error_bg;
+        fg = self.error_fg;
+        width = entry.width;
+    }
+
+    if (width > 0 and buffer.box_width > width) {
+        const label_y = buffer.box_y + buffer.margin_box_v;
+        const x = buffer.box_x + ((buffer.box_width - width) / 2);
+        TerminalBuffer.drawColorLabel(text, x, label_y, fg, bg);
+   }
 }
 
 pub fn clearRendered(allocator: std.mem.Allocator, buffer: TerminalBuffer) !void {
