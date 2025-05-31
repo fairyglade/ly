f983c6b19b15477acaa66de878b0e260094e6c44
AnErrupTion <anerruption@disroot.org>
2025-05-31 10:39:15 +0200

    Backport: Fix big clock UB in ReleaseSafe
    Signed-off-by: AnErrupTion <anerruption@disroot.org>

--- src/bigclock.zig
+++ src/bigclock.zig
@@ -34,24 +34,24 @@ pub fn alphaBlit(x: usize, y: usize, tb_width: usize, tb_height: usize, cells: [
     }
 }
 
-fn toBigNumber(char: u8, bigclock: Bigclock) []const u21 {
+fn toBigNumber(char: u8, bigclock: Bigclock) [SIZE]u21 {
     const locale_chars = switch (bigclock) {
         .fa => fa.locale_chars,
         .en => en.locale_chars,
         .none => unreachable,
     };
     return switch (char) {
-        '0' => &locale_chars.ZERO,
-        '1' => &locale_chars.ONE,
-        '2' => &locale_chars.TWO,
-        '3' => &locale_chars.THREE,
-        '4' => &locale_chars.FOUR,
-        '5' => &locale_chars.FIVE,
-        '6' => &locale_chars.SIX,
-        '7' => &locale_chars.SEVEN,
-        '8' => &locale_chars.EIGHT,
-        '9' => &locale_chars.NINE,
-        ':' => &locale_chars.S,
-        else => &locale_chars.E,
+        '0' => locale_chars.ZERO,
+        '1' => locale_chars.ONE,
+        '2' => locale_chars.TWO,
+        '3' => locale_chars.THREE,
+        '4' => locale_chars.FOUR,
+        '5' => locale_chars.FIVE,
+        '6' => locale_chars.SIX,
+        '7' => locale_chars.SEVEN,
+        '8' => locale_chars.EIGHT,
+        '9' => locale_chars.NINE,
+        ':' => locale_chars.S,
+        else => locale_chars.E,
     };
 }
